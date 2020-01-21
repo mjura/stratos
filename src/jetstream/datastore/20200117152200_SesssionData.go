189e5cbf5dae09c3d8564cbf3763cbc2ccf3d396
Neil MacDougall <nwmac@users.noreply.github.com>
2020-01-21 11:24:53 +0000

    Fix alignment

--- src/jetstream/datastore/20200117152200_SesssionData.go
+++ src/jetstream/datastore/20200117152200_SesssionData.go
@@ -12,7 +12,7 @@ func init() {
 		sessionDataTable += "  session           VARCHAR(255)      NOT NULL,"
 		sessionDataTable += "  groupName         VARCHAR(32)       NOT NULL,"
 		sessionDataTable += "  name              VARCHAR(64)       NOT NULL,"
-		sessionDataTable += "  value       	     TEXT              NOT NULL,"
+		sessionDataTable += "  value             TEXT              NOT NULL,"
 		sessionDataTable += "  keep_on_expire    BOOLEAN           NOT NULL DEFAULT FALSE,"
 		sessionDataTable += "  expired           BOOLEAN           NOT NULL DEFAULT FALSE);"
 
