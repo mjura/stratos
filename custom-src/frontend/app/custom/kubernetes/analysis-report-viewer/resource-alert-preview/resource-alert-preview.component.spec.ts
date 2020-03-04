82e98ba2742cd4f9cacce1c6bd2dbb164be462b8
Neil MacDougall <neil.macdougall@suse.com>
2020-03-04 20:14:35 +0000

    Final front-end unit test fixes

--- custom-src/frontend/app/custom/kubernetes/analysis-report-viewer/resource-alert-preview/resource-alert-preview.component.spec.ts
+++ custom-src/frontend/app/custom/kubernetes/analysis-report-viewer/resource-alert-preview/resource-alert-preview.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResourceAlertPreviewComponent } from './resource-alert-preview.component';
 import { ResourceAlertViewComponent } from './resource-alert-view/resource-alert-view.component';
+import { SidePanelService } from 'frontend/packages/core/src/shared/services/side-panel.service';
+import { KubernetesBaseTestModules } from '../../kubernetes.testing.module';
 
 describe('ResourceAlertPreviewComponent', () => {
   let component: ResourceAlertPreviewComponent;
@@ -9,7 +11,13 @@ describe('ResourceAlertPreviewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResourceAlertPreviewComponent, ResourceAlertViewComponent ]
+      declarations: [ ResourceAlertPreviewComponent, ResourceAlertViewComponent ],
+      imports: [
+        KubernetesBaseTestModules,
+      ],
+      providers: [
+        SidePanelService,
+      ]
     })
     .compileComponents();
   }));
