7c14287d31ed107b22500759d64b95fd7c564011
Richard Cox <richard.cox@suse.com>
2020-06-26 16:17:38 +0100

    Fix after merge

--- src/frontend/packages/core/src/features/endpoints/connect.service.ts
+++ src/frontend/packages/core/src/features/endpoints/connect.service.ts
@@ -1,5 +1,15 @@
 import { combineLatest, Observable, of, Subject, Subscription } from 'rxjs';
-import { delay, distinctUntilChanged, filter, map, pairwise, startWith, switchMap, tap } from 'rxjs/operators';
+import {
+  delay,
+  distinctUntilChanged,
+  filter,
+  map,
+  pairwise,
+  startWith,
+  switchMap,
+  tap,
+  withLatestFrom,
+} from 'rxjs/operators';
 
 import { AuthParams, ConnectEndpoint } from '../../../../store/src/actions/endpoint.actions';
 import { entityCatalog } from '../../../../store/src/entity-catalog/entity-catalog';
